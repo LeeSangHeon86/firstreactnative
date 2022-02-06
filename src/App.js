317624cc20d86c658606398eb9f52264b555935b
이상헌 <bleach8625@gmail.com>
2022-02-06 17:46:26 +0900

    End : Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import react, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 import MyButton from './MyButton';
 
 export default function App() {
+  const [add, setAdd] = useState(0);
+  const [multi, setMulti] = useState(1);
+
   return (
     <View style={styles.container}>
-      <Text>Hi, React Native111</Text>
-      <StatusBar style="auto" />
-      <Button title="Button" onPress={() => Alert.alert('click, click')} />
-      <MyButton />
+      <TextInput
+        // onChange={event => console.log(event.nativeEvent)}
+        onChangeText={text => console.log(text)}
+        style={{ borderWidth: 1, borderColor: 'black', fontSize: 20 }}
+      ></TextInput>
+      {/* <Text style={{ fontSize: 20 }}>{add}</Text>
+      <Text style={{ fontSize: 20 }}>{multi}</Text>
+      <MyButton
+        title="Add"
+        onPress={() => {
+          setAdd(add + 2);
+        }}
+      />
+      <MyButton
+        title="Multi"
+        onPress={() => {
+          setMulti(multi * 2);
+        }}
+      /> */}
     </View>
   );
 }
--- src/MyButton.js
+++ src/MyButton.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { TouchableOpacity, View, Text, Alert } from 'react-native';
+import PropTypes from 'prop-types';
 
-const MyButton = () => {
+const MyButton = ({ title, onPress, children }) => {
   return (
-    <TouchableOpacity onPress={() => Alert.alert('button click')}>
-      <View style={{ backgroundColor: 'green', padding: 10 }}>
-        <Text style={{ fontSize: 20, color: 'white' }}>MyButton</Text>
+    <TouchableOpacity
+      onPress={() => console.log('press')}
+      onPressIn={() => console.log('pressIn')}
+      onPressOut={() => console.log('pressOut')}
+      onLongPress={() => console.log('Long')}
+      delayLongPress={3000}
+    >
+      <View style={{ backgroundColor: 'green', padding: 10, margin: 10 }}>
+        <Text style={{ fontSize: 20, color: 'white' }}>
+          {children || title}
+        </Text>
       </View>
     </TouchableOpacity>
   );
 };
+
+MyButton.defaultProps = {
+  title: 'default',
+  onPress: () => Alert.alert('default'),
+};
+
+// MyButton.PropTypes = {
+//   title: PropTypes.string.isRequired, // 반드시 string 이여야 하며 데이터가 항상 있어야 한다.
+//   onPress: PropTypes.func,
+// };
 
 export default MyButton;
