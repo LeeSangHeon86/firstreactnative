8e3bf2fb7e83fab2a4aad5e37d0643d49f7eabf7
이상헌 <bleach8625@gmail.com>
2022-02-18 09:15:11 +0900

    hook:20220218

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
+import Counter from './components/Counter';
+import Form from './components/Form';
+import Button from './components/Button';
+import CoinInfo from './components/CoinInfo';
+import { useFetch } from './hooks/useFetch';
 
 const Container = styled.View`
   flex: 1;
-  background-color: ${theme.background};
+  background-color: white;
   align-items: center;
   justify-content: center;
 `;
 
+const LoadingText = styled.Text`
+  font-size: 30px;
+  color: red;
+`;
+
 export default function App() {
+  const URL = 'https://api.coinlore.net/api/tickers/?limit=3';
+  const { data, error, inProgress } = useFetch(URL);
+  console.log(data['data']);
+
   return (
     <ThemeProvider theme={theme}>
       <Container>
-        <Text>ToDo</Text>
-        <StatusBar style="auto" />
+        {inProgress && <LoadingText>Loading...</LoadingText>}
+        {data['data'].map(({ symbol, name, price_usd }) => {
+          return (
+            <CoinInfo
+              key={symbol}
+              symbol={symbol}
+              name={name}
+              price={price_usd}
+            />
+          );
+        })}
       </Container>
     </ThemeProvider>
   );
